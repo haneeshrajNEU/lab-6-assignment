e5e29b7ed7bee7bfd141ca03a5240f00dcc0597f
haneeshraj <haneeshraj.banisetti@gmail.com>
2024-03-20 14:24:24 -0400

    init: Initial Commit

---
+++ src/User.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author hanee
+ */
+public class User {
+     private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    private int Age;
+    private int id;
+}
